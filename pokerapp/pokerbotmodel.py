c8fc045fb999c23fbf400affdee6bb1db56a93a0
shahinmnm <119159633+shahinmnm@users.noreply.github.com>
2025-08-31 10:02:18 +0330

    Update pokerbotmodel.py

--- pokerapp/pokerbotmodel.py
+++ pokerapp/pokerbotmodel.py
@@ -241,21 +241,8 @@ class PokerBotModel:
 
             context.chat_data[KEY_OLD_PLAYERS] = list(
                 map(lambda p: p.user_id, game.players),
-
-        game.state = GameState.ROUND_PRE_FLOP
-        self._divide_cards(game=game, chat_id=chat_id)
-
-        self._round_rate.round_pre_flop_rate_before_first_turn(game)
-
-        num_players = len(game.players)
-        if num_players == 2:
-            game.current_player_index = -1 
-        else:
-            game.current_player_index = 1 
-
-        self._process_playing(chat_id=chat_id, game=game)
-
-        context.chat_data[KEY_OLD_PLAYERS] = [p.user_id for p in game.players]
+            )
+        # --- END OF CHANGES ---
 
     def _fast_forward_to_finish(self, game: Game, chat_id: ChatId):
         """ When no more betting is possible, reveals all remaining cards """
