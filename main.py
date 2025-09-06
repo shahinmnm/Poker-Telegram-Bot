059bcaa2991ac6de20416a1604ef3410e96874e2
shahinmnm <119159633+shahinmnm@users.noreply.github.com>
2025-09-06 21:00:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,6 @@ from dotenv import load_dotenv
 
 from pokerapp.config import Config
 from pokerapp.pokerbot import PokerBot
-
-
-def main() -> None:
-    load_dotenv()
-    cfg: Config = Config()
-
-    if cfg.TOKEN == "":
-        print("Environment varaible POKERBOT_TOKEN is not set")
-        exit(1)
-
-    bot = PokerBot(token=cfg.TOKEN, cfg=cfg)
-    bot.run()
     import logging
     from pokerapp.pokerbot import PokerBot
     from pokerapp.config import Config
@@ -29,6 +17,18 @@ def main() -> None:
         cfg = Config()
         bot = PokerBot(cfg)
         bot.run()
+
+def main() -> None:
+    load_dotenv()
+    cfg: Config = Config()
+
+    if cfg.TOKEN == "":
+        print("Environment varaible POKERBOT_TOKEN is not set")
+        exit(1)
+
+    bot = PokerBot(token=cfg.TOKEN, cfg=cfg)
+    bot.run()
+
 
 
 if __name__ == "__main__":
